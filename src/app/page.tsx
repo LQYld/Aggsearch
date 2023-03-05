65d4954e9a430d67efb7655e4cff86dd0173f80f
LQYld <54627537+LQYld@users.noreply.github.com>
2023-03-05 12:52:14 +0800

    feat: add stylelint and prettierr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline text-red-800">
-    Hello, Next.js 111222!
-  </h1>
+      Hello, Next.js 111222!
+    </h1>
   )
 }
