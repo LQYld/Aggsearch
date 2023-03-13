2bccc55104d20b7c75e95a7b6c864946495f049c
LQYld <54627537+LQYld@users.noreply.github.com>
2023-03-13 23:34:45 +0800

    feat: add pages  header components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,7 @@ export default function Header() {
   return (
     <div className="flex items-center justify-between">
       <div></div>
-      <div>
-        <Button onClick={() => Toast.warning({ content: 'welcome' })}>
-          Hello Semi
-        </Button>
-      </div>
+      <div></div>
     </div>
   )
 }
