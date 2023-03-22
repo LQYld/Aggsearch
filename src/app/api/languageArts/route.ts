adedfb72ebed52e3171f9e2f53734c0359605846
LQYld <54627537+LQYld@users.noreply.github.com>
2023-03-22 09:22:40 +0800

    feat: finish api (languageArts)

--- src/app/api/languageArts/route.ts
+++ src/app/api/languageArts/route.ts
@@ -5,17 +5,22 @@ export async function GET() {
   const month = ('0' + (time.getMonth() + 1)).slice(-2) //月
   const day = ('0' + time.getDate()).slice(-2) //日
   const mydate = year + '-' + month + '-' + day
-  const fetchResponse = await fetch(
+  const todayPoetryResponse = fetch(
     `${BASE_THIRD_PARTY_INTERFACE_ADDRESS.TODAY_POETRY}one.json?client=npm-sdk/1.0`
   )
-  const fetchResponse2 = await fetch(
+  const everydayEnglishResponse = fetch(
     `${BASE_THIRD_PARTY_INTERFACE_ADDRESS.EVERYDAY_ENGLISH}index.php?callback=cb&c=dailysentence&m=getdetail&title=${mydate}`
   )
-  console.log(fetchResponse2)
+  const promiseList = [todayPoetryResponse, everydayEnglishResponse]
+
+  const promiseListResponse = await Promise.all(promiseList)
+  const everydayEnglishRegex = /(?<=cb\()(.+?)(?=\))/g //正则剔除 cb() 小括号
   return new Response(
     JSON.stringify({
-      [`zh-cn`]: await fetchResponse.json(),
-      en: await fetchResponse2
+      [`zh-cn`]: await promiseListResponse[0].json(),
+      en: JSON.parse(
+        (await promiseListResponse[1].text()).match(everydayEnglishRegex)[0]
+      )
     })
   )
 }
