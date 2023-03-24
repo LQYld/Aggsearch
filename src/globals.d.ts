e45c998fe48f6e7a3ff42dbe56600c4e5286a056
LQYld <54627537+LQYld@users.noreply.github.com>
2023-03-24 09:40:17 +0800

    feat: add weather-icon-component

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -5,3 +5,9 @@ declare module '*.css' {
   const css: stylesType
   export default css
 }
+declare module '*.svg' {
+  import React = require('react')
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
+}
