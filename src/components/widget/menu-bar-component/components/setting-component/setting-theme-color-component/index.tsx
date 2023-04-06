f51cea899586e42529cbd9ba519e1b3100093a00
LQYld <54627537+LQYld@users.noreply.github.com>
2023-04-07 01:01:50 +0800

    feat: add setting-theme-component colorConfigList UI

--- src/components/widget/menu-bar-component/components/setting-component/setting-theme-color-component/index.tsx
+++ src/components/widget/menu-bar-component/components/setting-component/setting-theme-color-component/index.tsx
@@ -1,4 +1,5 @@
 import { Typography, Divider, Banner } from '@douyinfe/semi-ui'
+import { colorConfigList } from './config'
 import styles from './styles.module.css'
 const { Title } = Typography
 export default function SettingThemeColorComponent() {
@@ -16,8 +17,52 @@ export default function SettingThemeColorComponent() {
         </Title>
       </div>
       <Divider dashed={true} margin="12px" />
-      <div className={`${styles['theme-box']} current-theme`}></div>
+      <div
+        className={`${styles['theme-box']} current-theme ml-2 ${styles['base-box']} text-gray-50`}
+      >
+        Primary
+      </div>
       <Divider margin="12px" />
+      <div className={`${styles['color-list']}`}>
+        {colorConfigList.map((itemColor, itemColorIndex) => {
+          return (
+            <div
+              key={`colorConfigList_${itemColorIndex}`}
+              className={`mb-4 ml-2 flex flex-col items-center`}
+            >
+              {itemColor.colorList.map((node, nodeIndex) => {
+                return (
+                  <div
+                    key={`colorConfigList_itemColor_${nodeIndex}`}
+                    className={`${styles['base-box']} text-xs font-semibold ${
+                      nodeIndex >= itemColor.lightNum
+                        ? 'text-gray-50'
+                        : 'text-gray-900'
+                    }`}
+                    style={{
+                      backgroundColor: `${node.value}`
+                    }}
+                  >
+                    {node.label}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+      <Divider margin="12px" />
+      <div>
+        <Title type="secondary" heading={6}>
+          {'Default settings'}
+        </Title>
+      </div>
+      <Divider dashed={true} margin="12px" />
+      <div
+        className={`${styles['theme-box']} current-theme ml-2 ${styles['base-box']} text-gray-50`}
+      >
+        Primary
+      </div>
     </div>
   )
 }
